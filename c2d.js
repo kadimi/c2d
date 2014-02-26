04a1350cc8c754c3b0099fe3252f76afddd2c033
Nabil Kadimi <nabil@kadimi.com>
2014-02-26 11:34:39 +0000

    Refactor dc to c2d

--- c2d.js
+++ c2d.js
@@ -4,17 +4,17 @@ jQuery(document).ready(function ($) {
 
     $("[class^='c2d_'],div[class*=' c2d_']").each(function () {
 
-        var dc, c2d_array = [], declaration;
+        var c2d, c2d_array = [], declaration;
         var classes = $(this).attr('class');
         var c2d_regex = /\bc2d_[a-zA-Z0-9_]+_[a-zA-Z0-9_]+\b/g;
         
-        while( dc = c2d_regex.exec(classes) ){
-            c2d_array.push(dc[0]);
+        while( c2d = c2d_regex.exec(classes) ){
+            c2d_array.push(c2d[0]);
         }
 
         for (var i = 0; i < c2d_array.length; i++) {
-            dc = c2d_array[i];
-            declaration = /\bc2d_(.+)_(.+)\b/.exec(dc);
+            c2d = c2d_array[i];
+            declaration = /\bc2d_(.+)_(.+)\b/.exec(c2d);
             declaration['property'] = declaration[1];
             declaration['value'] = declaration[2];
             $(this).css(declaration['property'], declaration['value']);
